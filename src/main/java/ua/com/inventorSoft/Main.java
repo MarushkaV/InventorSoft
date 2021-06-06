2bdd467fb0b51e4c3bccbe07a3ff168462372136
VolodymyrMarushka <vvolodimirr.m@gmail.com>
2021-06-06 16:07:52 +0300

    Added comments to algorithm

--- src/main/java/ua/com/inventorSoft/Main.java
+++ src/main/java/ua/com/inventorSoft/Main.java
@@ -3,7 +3,7 @@ package ua.com.inventorSoft;
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.println(arrayChallenge(new int[]{1, 5,10,10}));
+		System.out.println(arrayChallenge(new int[]{1, 5, 10, 10}));
 
 	}
 
@@ -13,19 +13,25 @@ public class Main {
 		if (length == 1 || arr[length - 1] == 1) {
 			return -1;
 		}
+		//define how many rows of pascal triangle to build.
+		// For instance if second number entered row is 5 then we should build 6 rows
 		int row = arr[1] + 1;
+		// define index of last number entered row.
 		int lastNumber = arr.length - 1;
 
 		int[][] triangle = new int[row][row];
 
-		for (int line = 0; line < row; line++) {
-			for (int i = 0; i <= line; i++) {
-				if (line == i || i == 0) {
-					triangle[line][i] = 1;
+		//building pascal's triangle
+		for (int column = 0; column < row; column++) {
+			for (int i = 0; i <= column; i++) {
+				if (column == i || i == 0) {
+					triangle[column][i] = 1;
 				} else {
-					triangle[line][i] = triangle[line - 1][i - 1] + triangle[line - 1][i];
+					triangle[column][i] = triangle[column - 1][i - 1] + triangle[column - 1][i];
 				}
-				if (line == row - 1 && i == lastNumber + 1) {
+
+				//checks if particular row and index is number we need
+				if (column == row - 1 && i == lastNumber + 1) {
 					return triangle[row - 1][lastNumber + 1];
 				}
 			}
